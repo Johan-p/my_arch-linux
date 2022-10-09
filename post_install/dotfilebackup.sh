8387fe9946c21bd63a14804f863b0b756104729f
Johan-p <johan.vincent.postma@gmail.com>
2022-10-09 22:15:35 +0200

    post install dotfiles backup script

---
+++ post_install/dotfilebackup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cp ~/.bash_profile /home/johan/Documents/my_arch-linux/desktop_install/.bash_profile
+echo "Copied .bash_profile"
+cp ~/.bashrc /home/johan/Documents/my_arch-linux/desktop_install/.bashrc
+echo "Copied .bashrc"
+cp ~/.config/i3/config /home/johan/Documents/my_arch-linux/desktop_install/.config/i3/config
+echo "Copied i3 config"
+cp ~/.Xdefaults /home/johan/Documents/my_arch-linux/desktop_install/.Xdefaults
+echo "Copied .Xdefaults"
+cp ~/.urxvt/ext/solarized /home/johan/Documents/my_arch-linux/desktop_install/.urxvt/ext/solarized
+echo "Copied urxvt extension scripts"
+cp ~/.xinitrc /home/johan/Documents/my_arch-linux/desktop_install/.xinitrc
+echo "Copied xinitrc"
